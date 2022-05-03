36360d1bb3bf96610709d30bb4a4e3a47de07499
Ancil Trent <trent12@purdue.edu>
2022-05-02 23:35:57 -0400

    Transactionalized backend queries

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,51 +5,60 @@ const Role = db.role;
 const Op = db.Sequelize.Op;
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
-exports.signup = (req, res) => {
+const {Transaction} = require('sequelize');
+const { sequelize } = require("../models");
+exports.signup = async(req, res) => {
   // Save User to Database
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
-  })
-    .then(user => {
-      res.send({ message: "User was registered successfully!" });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+  const t = await sequelize.transaction();
+  try {
+    await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8)
+    }, {transaction: t})
+
+    await t.commit();
+    res.send({ message: "User was registered successfully!" });
+  } catch (err) {
+    await t.rollback();
+    res.status(500).send({ message: err.message });
+  }
 };
-exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      username: req.body.username
+exports.signin = async(req, res) => {
+  const t = await sequelize.transaction();
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }, {transaction: t});
+
+    await t.commit();
+
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
     }
-  })
-    .then(user => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!"
-        });
-      }
-      var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
+    var passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!"
       });
-      res.status(200).send({
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        accessToken: token
-      });
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message });
+    }
+    var token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400 // 24 hours
     });
+    res.status(200).send({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      accessToken: token
+    });
+  } catch (err) {
+    await t.rollback();
+    res.status(500).send({ message: err.message });
+  }
 };
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,13 +1,21 @@
+const { sequelize } = require("../models");
 const db = require("../models");
 const User = db.user;
 
 exports.allAccess = (req, res) => {
     res.status(200).send("Public Content.");
 };
-exports.groupBoard = (req, res) => {
-    User.findByPk(req.userId).then(user => {
-        user.getGroups().then(groups => {
-            res.status(200).send(groups);
-        })
-    })
+exports.groupBoard = async(req, res) => {
+    const t = await sequelize.transaction();
+    try {
+        const user = await User.findByPk(req.userId, {transaction: t});
+
+        const groups = await user.getGroups({transaction: t});
+
+        await t.commit();
+        res.status(200).send(groups);
+    } catch (err) {
+        await t.rollback();
+        res.status(500).send({message: err.message});
+    }
 };
