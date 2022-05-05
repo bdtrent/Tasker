c161289bda0099cdbacb983248c05f5737d23edd
Ancil Trent <trent12@purdue.edu>
2022-05-04 23:15:32 -0400

    Transactionalized task.controller

--- app/controllers/task.controller.js
+++ app/controllers/task.controller.js
@@ -1,11 +1,9 @@
-const { sequelize, user } = require("../models");
+const { sequelize } = require("../models");
 const db = require("../models");
 
-const List = db.list;
-const Op = db.Sequelize.Op;
 const Task = db.task;
-const User = db.user;
 const Group = db.group;
+const {Transaction} = require('sequelize');
 
 exports.getTaskForMonth = async (req, res) => {
 	if (!('year' in req.query && 'month' in req.query)) {
@@ -21,65 +19,95 @@ exports.getTaskForMonth = async (req, res) => {
 }
 
 exports.getUsers = (req, res) => {
-  Task.findByPk(req.query.task).then(task => {
-    task.getUsers().then(users => {
-      res.status(200).send(users);
-    })
-  }).catch(err => {
+  const t = await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED});
+  try {
+    const task = await Task.findByPk(req.query.task, {
+      transaction: t
+    });
+    const users = await task.getUsers({transaction: t});
+    await t.commit();
+    res.status(200).send(users);
+  } catch (err) {
+    await t.rollback();
     res.status(500).send({message: err.message});
-  })
+  }
 }
 
 exports.getGroupTasks = (req,res) => {
-  Task.findAll({
-    where: {
-      groupId: req.query.group
-    }
-  }).then(tasks => {
+  const t = await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED});
+  try {
+    const task = await Task.findAll({
+      where: {
+        groupId: req.query.group
+      }
+    }, {transaction: t});
+
+    await t.commit();
     res.status(200).send(tasks);
-  }).catch(err => {
+  } catch (err) {
+    await t.rollback();
     res.status(500).send({message: err.message});
-  })
+  }
 }
 
 exports.create = (req, res) => {
-  const group = Group.findByPk(req.body.group);
-   Task.create({
-    name: req.body.name,
-    due_date: req.body.date,
-    owner_name: req.body.owner
-   }).then(task => {
-     group.addTask(task);
-     res.status(200).send({message: "Task created successfully!"});
-   }).catch(err => {
-     res.status(500).send({message: err.message});
-   })
+  const t = await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE});
+  t.afterCommit(() => {
+    res.status(200).send({message: "Task created successfully!"});
+  })
+  try {
+    const task = await Task.create({
+      name: req.body.name,
+      due_date: req.body.date,
+      owner_name: req.body.owner
+     }, {transaction: t});
+
+     const group = await Group.findByPk(req.body.group, {transaction: t});
+
+     await group.addTask(task);
+
+     await t.commit();
+  } catch (err) {
+    await t.rollback();
+    res.status(500).send({message: err.message});
+  }
 };
 
 exports.update = (req, res) => {
-    Task.findByPk(req.body.task).then( task => {
-      task.update({
-        name: req.body.name,
-        due_date: req.body.date
-      });
-      res.status(200).send({message: "Task updated successfully!"});
-    }).catch(err => {
-      res.status(500).send({message: err.message});
-    })
+  const t = await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED});
+  t.afterCommit(() => {
+    res.status(200).send({message: "Task updated successfully!"});
+  })
+  try {
+    const task = await Task.findByPk(req.body.task, {transaction: t});
+
+    await task.update({
+      name: req.body.name,
+      due_date: req.body.date
+    }, {transaction: t});
+
+    await t.commit();
+  } catch (err) {
+    await t.rollback();
+    res.status(500).send({message: err.message});
+  }
 };
 
 exports.delete = (req, res) => {
-    Task.delete({
+  const t = await sequelize.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED});
+  try {
+    await Task.destroy({
       where: {
         id: req.body.task
       }
-    }).then(response => {
-      res.status(200).send({
-        message: "Task deleted successfully!"
-      });
-    }).catch(err => {
-      res.status(500).send({
-        message: err.message
-      });
-    })
+    }, {transaction: t});
+
+    await t.commit();
+    res.status(200).send({message: "Task was deleted!"});
+  } catch (err) {
+    await t.rollback();
+    res.status(500).send({
+      message: err.message
+    });
+  }
 };
